675cb865f721c7c07f3f0abd5f483ed9f5a76029
Cynthia-Matsitsa <cynthiamatsitsa@gmail.com>
2024-03-22 16:41:17 +0300

    add file

---
+++ array.js
@@ -0,0 +1,21 @@
+//find the last element array.
+let arr1 = [3,7,3,4,90,12];
+console.log(arr1.slice(-1));
+
+let arr2 = [true,"green","where",12,56];
+console.log(arr2.slice(-1));
+
+//js program that joins array.
+const myPests = ("Cow","Bird","Snake","Dog");
+let animals =  myPests.toString();
+console.log({animals});
+
+//js script to sort the array.
+const arr3 = [-5,9,5,3,2,-3,6,8.4,-1];
+let num = arr3.sort();
+console.log({num});
+
+//js script to sort the array.
+let word = ("renniw");
+let k = word.sort();
+console.log({k}); 
